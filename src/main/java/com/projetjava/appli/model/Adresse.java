63d6b04b4dfd41266daa7d148b1539d507a0162f
Fanokiller <44086700+Fanokiller@users.noreply.github.com>
2020-04-22 11:28:22 +0200

    Ajout des models

---
+++ src/main/java/com/projetjava/appli/model/Adresse.java
@@ -0,0 +1,47 @@
+package com.projetjava.appli.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table( name = "adresse")
+public class Adresse {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) int id;
+    private int postalNumber;
+    private String street;
+    private int number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostalNumber() {
+        return postalNumber;
+    }
+
+    public void setPostalNumber(int postalNumber) {
+        this.postalNumber = postalNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
