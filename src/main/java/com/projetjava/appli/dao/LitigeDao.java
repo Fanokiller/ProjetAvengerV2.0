0630b721d27012750463434dc47bcdf4c9df6346
Fanokiller <44086700+Fanokiller@users.noreply.github.com>
2020-04-22 15:32:32 +0200

    Ajout des models

--- src/main/java/com/projetjava/appli/dao/LitigeDao.java
+++ src/main/java/com/projetjava/appli/dao/LitigeDao.java
@@ -1,5 +1,6 @@
 package com.projetjava.appli.dao;
 
+import com.projetjava.appli.model.Litige;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/projetjava/appli/dao/PaysDAO.java
+++ src/main/java/com/projetjava/appli/dao/PaysDAO.java
@@ -1,9 +1,9 @@
 package com.projetjava.appli.dao;
 
-import com.projetjava.appli.model.Country;
+import com.projetjava.appli.model.Pays;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryDAO extends JpaRepository<Country, Integer> {
+public interface PaysDAO extends JpaRepository<Pays, Integer> {
 }
