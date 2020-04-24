1f2a777a180ce8e41f6b414729e3f3e119fab21f
Fanokiller <44086700+Fanokiller@users.noreply.github.com>
2020-04-24 09:05:31 +0200

    Continuité des view

---
+++ src/main/java/com/projetjava/appli/controller/IdentificationController.java
@@ -0,0 +1,35 @@
+package com.projetjava.appli.controller;
+
+import com.projetjava.appli.dao.IdentificationDAO;
+import com.projetjava.appli.model.Identification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IdentificationController {
+
+    @Autowired
+    IdentificationDAO identificationDAO;
+
+    @GetMapping("/inscription")
+    public String inscription(Model model) {
+
+
+        model.addAttribute("titre", "Inscription");
+        model.addAttribute("identifiant", identificationDAO.findAll());
+
+        return "Inscription";
+    }
+
+    @GetMapping("/connexion")
+    public String connexion(Model model) {
+
+
+        model.addAttribute("titre", "Connexion");
+        model.addAttribute("identifiant", identificationDAO);
+
+        return "Connexion";
+    }
+}
